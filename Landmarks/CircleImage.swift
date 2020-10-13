6bd6720f0ad6ddfbd9c1cb7c736dea94f68d2229
Joshua Zierman <github@zierman.us>
2020-10-13 16:41:28 -0500

    Add a overlay border Creating and Combining Views - Section 4 step 5: Create another circle with a gray stroke, and then add it as an overlay to give the image a border.

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
     }
 }
 
