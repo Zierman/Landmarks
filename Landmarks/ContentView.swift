192d33e0b841a1f442abc044dc4e8488fa037460
Joshua Zierman <github@zierman.us>
2020-10-13 17:16:29 -0500

    Add MapView to ContentView Creating and Combining Views - Section 6 step 3: Add your custom MapView to the top of the stack. Set the size of the MapView with frame(width:height:).

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,19 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
+            MapView()
+                .frame(height: 300)
+            
             VStack (alignment: .leading) {
+                
                 Text("Turtle Rock")
                     .font(.title)
                     .padding()
+                
                 HStack {
+                    
                     Text("Joshua Tree National Park")
                         .font(.subheadline)
+                    
                     Spacer()
+                    
                     Text("California")
                         .font(.subheadline)
                 }
             }
             .padding()
+            
         }
     }
 }
