48875548be0ef084f0ff4e8fecdd4e7bcee60cb2
Joshua Zierman <github@zierman.us>
2020-10-13 17:18:27 -0500

    Add the CircleImage to ContentView Creating and Combining Views - Section 6 step 5: Add the CircleImage view to the stack.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
             MapView()
                 .frame(height: 300)
             
+            CircleImage()
+            
             VStack (alignment: .leading) {
                 
                 Text("Turtle Rock")
