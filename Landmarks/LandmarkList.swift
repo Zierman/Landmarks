30b39928be272e3edfb5734e5b64a7f2633416f3
Joshua Zierman <github@zierman.us>
2020-10-15 17:07:40 -0500

    Create the LandmarkList.swift file Building Lists and Navigation - Section 4 Step 1: Create a new SwiftUI view, named LandmarkList.swift.

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Joshua Zierman on 10/15/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
