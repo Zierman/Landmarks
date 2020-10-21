2ba4377ee6e3d128a49953ac1e88034731255199
Joshua Zierman <github@zierman.us>
2020-10-20 20:17:17 -0500

    Set up to show multiple previews Building Lists and Navigation - Section 8 Step 2: Within the list preview, embed the LandmarkList in a ForEach instance, using an array of device names as the data.

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -22,7 +22,9 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
     }
 }
