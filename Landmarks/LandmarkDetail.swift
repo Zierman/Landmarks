94e8bab0a63db0e0fb1f176269b40d1b4884fdaf
Joshua Zierman <github@zierman.us>
2020-10-22 14:13:04 -0500

    Added spacer and padding to improve the visual arrangement

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -33,6 +33,8 @@ struct LandmarkDetail: View {
                         .font(.title)
                         .padding()
                     
+                    Spacer()
+                    
                     Button(action: {
                         self
                             .userData
@@ -55,6 +57,7 @@ struct LandmarkDetail: View {
                             
                         }
                     }
+                    .padding()
                 }
                 HStack {
                     
